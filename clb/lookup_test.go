f9a84bfcb4007eccaff8b124a2f5cf116c08cc5b
Ben Schwartz <benschw@gmail.com>
2014-05-28 11:13:19 -0500

    test(example): adding example lookup to tests

--- clb/lookup_test.go
+++ clb/lookup_test.go
@@ -9,23 +9,18 @@ import (
 var _ = fmt.Print // For debugging; delete when done.
 var _ = log.Print // For debugging; delete when done.
 
-// func Test_LookupConsul_ReturnsConsulAddress(t *testing.T) {
-// 	// given
-// 	port := "8300"
+func ExampleLookup() {
+	server := DNSServer{Address: "8.8.8.8", Port: "53"}
 
-// 	// when
-// 	srvName := "consul.service.consul"
-// 	address, err := LookupAddress(srvName)
+	address, err := LookupAddress(server, "_xmpp-server._tcp.google.com")
 
-// 	// then
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	if port != address.Port {
-// 		t.Errorf("port %s not expected", address.Port)
-// 	}
-// 	fmt.Printf("%+v", address)
-// }
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v", address)
+	// Output: {Address:74.125.142.put:  Port:5269}
+}
 
 func Test_LookupGoogleXmppService_ReturnsAddress(t *testing.T) {
 	// given
