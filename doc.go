df4fb3c9999b6db1bb0700599d723788046208b4
Ben Schwartz <benschw@gmail.com>
2014-05-29 12:44:59 -0500

    doc(clb): got examples working and adding docs

--- doc.go
+++ doc.go
@@ -1,10 +1,8 @@
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 /*
 consul-clb-go is a library to support client side loadbalancing between
 services exposed via. consul with its custom DNS server.
+
+Go to http://godoc.org/github.com/benschw/consul-clb-go/clb for details
 
 */
 package main
