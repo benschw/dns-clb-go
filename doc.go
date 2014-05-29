68e4667c646c5aeb7a44ec3ab4bccf374fea684c
Ben Schwartz <benschw@gmail.com>
2014-05-29 11:52:38 -0500

    doc(doc.go): adding the start of documentation

--- doc.go
+++ doc.go
@@ -1,5 +1,10 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 /*
 consul-clb-go is a library to support client side loadbalancing between
 services exposed via. consul with it's custom DNS server.
 
 */
+package main
