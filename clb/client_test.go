985e1cedc9221959fe4495b746b5fca61d4c6665
Ben Schwartz <benschw@gmail.com>
2014-05-29 12:20:42 -0500

    style(tests): rearranging tests

--- clb/client_test.go
+++ clb/client_test.go
@@ -10,47 +10,6 @@ import (
 var _ = fmt.Print // For debugging; delete when done.
 var _ = log.Print // For debugging; delete when done.
 
-func ExampleRoundRobin() {
-	srvName := "foo.service.fliglio.com"
-	c := NewRoundRobinClb("8.8.8.8", "53")
-	address, err := c.GetAddress(srvName)
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	if address.Port == 8001 {
-		fmt.Printf("%s", address)
-	} else {
-		address2, err := c.GetAddress(srvName)
-		if err != nil {
-			fmt.Print(err)
-		}
-		fmt.Printf("%s", address2)
-	}
-	// Output: 0.1.2.3:8001
-
-}
-func ExampleRoundRobinFacade() {
-	srvName := "foo.service.fliglio.com"
-	c := NewClb("8.8.8.8", "53", RoundRobin)
-	address, err := c.GetAddress(srvName)
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	if address.Port == 8001 {
-		fmt.Printf("%s", address)
-	} else {
-		address2, err := c.GetAddress(srvName)
-		if err != nil {
-			fmt.Print(err)
-		}
-		fmt.Printf("%s", address2)
-	}
-	// Output: 0.1.2.3:8001
-
-}
-
 func doStuff(c LoadBalancer) error {
 	srvName := "foo.service.fliglio.com"
 	_, err := c.GetAddress(srvName)
