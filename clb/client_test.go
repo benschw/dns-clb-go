60fb208623cda80262e51a89b9a39e4c97d5c53e
Ben Schwartz <benschw@gmail.com>
2014-05-29 12:39:17 -0500

    style(tests): rearranging tests

--- clb/client_test.go
+++ clb/client_test.go
@@ -11,7 +11,7 @@ var _ = fmt.Print // For debugging; delete when done.
 var _ = log.Print // For debugging; delete when done.
 
 // Example with direct usage
-func ExampleRoundRobin() {
+func Example() {
 	srvName := "foo.service.fliglio.com"
 	c := NewRoundRobinClb("8.8.8.8", "53")
 	address, err := c.GetAddress(srvName)
