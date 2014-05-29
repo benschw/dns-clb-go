fb172e8609738e6bd773540c89e043a2f9fdd70b
Ben Schwartz <benschw@gmail.com>
2014-05-29 13:43:25 -0500

    test(random): adding test back in, wasn't getting tracked somehow

---
+++ randomclb/random_test.go
@@ -0,0 +1,33 @@
+package randomclb
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+var _ = fmt.Print // For debugging; delete when done.
+var _ = log.Print // For debugging; delete when done.
+
+//strconv.FormatInt(int64(srv.Port), 10)
+func TestRandomLookup(t *testing.T) {
+	// given
+	srvName := "foo.service.fliglio.com"
+	c := NewRandomClb("8.8.8.8", "53")
+
+	// when
+	address, err := c.GetAddress(srvName)
+
+	// then
+	if err != nil {
+		t.Error(err)
+	}
+
+	if address.Port == 8001 && address.Address == "0.1.2.3" {
+		return
+	} else if address.Port == 8002 && address.Address == "4.5.6.7" {
+		return
+	} else {
+		t.Errorf("port '%d' not expected with address: '%s'", address.Port, address.Address)
+	}
+}
