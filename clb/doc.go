6b58e557b5290a467c9ceaf5faccb2b4321bf38c
Ben Schwartz <benschw@gmail.com>
2014-05-31 12:17:51 -0500

    refactor(names): updating package names for repo fork

--- clb/doc.go
+++ clb/doc.go
@@ -1,6 +1,6 @@
 /*
-consul-clb-go is a library to support client side loadbalancing between
-services exposed via. consul with its custom DNS server.
+dns-clb-go is a library to support client side loadbalancing between
+services exposed via. consul / skydns with its custom DNS server.
 
 The clb package is your entry point for getting a particular load balancer
 algorithm to include in your application
--- doc.go
+++ doc.go
@@ -1,8 +1,8 @@
 /*
-consul-clb-go is a library to support client side loadbalancing between
+dns-clb-go is a library to support client side loadbalancing between
 services exposed via. consul with its custom DNS server.
 
-Go to http://godoc.org/github.com/benschw/consul-clb-go/clb for details
+Go to http://godoc.org/github.com/benschw/dns-clb-go/clb for details
 
 */
 package main
