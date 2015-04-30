3722281c7081342252ae906b21a6ea96257e8fa4
Ben Schwartz <benschw@gmail.com>
2015-04-30 10:50:56 -0500

    fixing bug

--- roundrobinclb/roundrobin_test.go
+++ roundrobinclb/roundrobin_test.go
@@ -36,30 +36,3 @@ func TestRoundRobinLookup(t *testing.T) {
 
 }
 
-func TestRoundRobinLookupMany(t *testing.T) {
-	// given
-	srvName := "foo.service.fliglio.com"
-	lib := dns.NewLookupLib("8.8.8.8:53")
-	c := NewRoundRobinClb(lib)
-
-	// when
-	address, err := c.GetAddress(srvName)
-	address2, err := c.GetAddress(srvName)
-	address3, err := c.GetAddress(srvName)
-	address4, err := c.GetAddress(srvName)
-	address5, err := c.GetAddress(srvName)
-
-	// then
-	if err != nil {
-		t.Error(err)
-	}
-
-	if address.Port == 8001 && address.Address == "0.1.2.3" {
-		return
-	} else if address.Port == 8002 && address.Address == "4.5.6.7" {
-		return
-	} else {
-		t.Errorf("port '%d' not expected with address: '%s'", address.Port, address.Address)
-	}
-
-}
